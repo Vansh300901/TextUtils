76d2bf2559e147a90c6885a523257a4b342df221
Vansh300901 <98076578+Vansh300901@users.noreply.github.com>
2024-06-10 16:22:48 -0700

    LowerCase button + NavBar dark
    Added another button to the textArea which lets the user to convert the inputted text into all lowercase.
    Also changed the color of the NavBar to dark

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -13,14 +13,29 @@ export default function TextForm(props) {
         setTextarea(newText)
     }
 
+    const handleLowercaseClick = () => {
+        let newText = textarea.toLowerCase();
+        setTextarea(newText)
+    }
+
     return (
-        <div>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-                <textarea className="form-control" id="myBox" rows="8" onChange={handleTextChange} value={textarea} placeholder='Enter Text Here'></textarea>
+        <>
+            <div className='container'>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" id="myBox" rows="8" onChange={handleTextChange} value={textarea} placeholder='Enter Text Here'></textarea>
+                </div>
+                <button className="btn btn-outline-primary mx-2" onClick={handleUppercaseClick}>Convert to Uppercase</button>
+                <button className="btn btn-outline-success mx-2" onClick={handleLowercaseClick}>Convert to Lowercase</button>
             </div>
-            <button className="btn btn-primary" onClick={handleUppercaseClick}>Convert to Uppercase</button>
-        </div>
+            <div className="container my-3">
+                <h2>Your Text Summary</h2>
+                <p>{textarea.length} characters and {textarea.split(" ").length} words</p>
+                <p>{0.008 * textarea.split(" ").length} minutes to read</p>
+                <h3>Preview</h3>
+                <p>{textarea}</p>
+            </div>
+        </>
     )
 }
 
