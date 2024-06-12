51cc198f05838dda34f8763a34e9dca4a5a14ec1
Vansh300901 <98076578+Vansh300901@users.noreply.github.com>
2024-06-12 14:52:19 -0700

    Added Alerts to buttons
    Added alerts to all the possible user interaction with the application to let the user know about the state of their request/action, and whether it was successful or not.

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -3,12 +3,25 @@ import NavBar from './components/NavBar';
 import TextForm from './components/TextForm';
 import About from './components/About';
 import { useState } from 'react';
+import Alert from './components/Alert';
 
 function App() {
   const [mode, setMode] = useState({
     backgroundColor: 'dark',
     darkModeText: 'Enable Dark Mode'
   });
+
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      message: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
 
   const toggleDarkMode = () => {
     if (mode.backgroundColor === 'dark') {
@@ -17,6 +30,7 @@ function App() {
         darkModeText: 'Enable Light Mode'
       })
       document.body.style.backgroundColor = 'black'
+      showAlert("Dark mode has been enabled", 'success')
     }
     else {
       setMode({
@@ -24,13 +38,15 @@ function App() {
         darkModeText: 'Enable Dark Mode'
       })
       document.body.style.backgroundColor = 'white'
+      showAlert("Light mode has been enabled", 'success')
     }
   }
   return (
     <div className="App">
       <NavBar title="TextUtils" mode={mode} toggleDarkMode={toggleDarkMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm heading="Enter Your Text To Analyze Below" mode={mode} />
+        <TextForm heading="Enter Your Text To Analyze Below" mode={mode} showAlert={showAlert} />
       </div>
       {/* <div className="container my-3">
         <About />
--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -11,24 +11,29 @@ const TextForm = (props) => {
     const handleUppercaseClick = () => {
         let newText = textarea.toUpperCase();
         setTextarea(newText)
+        props.showAlert("Successfully converted to Uppercase", "success")
     }
 
     const handleLowercaseClick = () => {
         let newText = textarea.toLowerCase();
         setTextarea(newText)
+        props.showAlert("Successfully converted to Lowercase", "success")
     }
 
     const handleClearClick = () => {
         setTextarea('')
+        props.showAlert("Successfully cleared the textbox", "success")
     }
 
     const handleCopyClick = () => {
         navigator.clipboard.writeText(textarea)
+        props.showAlert("Successfully copied to Clipboard", "success")
     }
 
     const handleRemoveSpacesClick = () => {
         let newText = textarea.split(/[ ]+/);
         setTextarea(newText.join(" "))
+        props.showAlert("Successfully removed extra spaces", "success")
     }
 
     useEffect(() => {
@@ -53,17 +58,17 @@ const TextForm = (props) => {
                 <div className="mb-3">
                     <textarea className="form-control" id="myBox" rows="8" onChange={handleTextChange} value={textarea} placeholder='Enter Text Here' style={{ backgroundColor: props.mode.backgroundColor === 'light' ? 'black' : 'white', color: props.mode.backgroundColor === 'light' ? 'white' : 'black' }}></textarea>
                 </div>
-                <button className="btn btn-primary mx-2" onClick={handleUppercaseClick}>Convert to Uppercase</button>
-                <button className="btn btn-success mx-2" onClick={handleLowercaseClick}>Convert to Lowercase</button>
-                <button className="btn btn-dark mx-2" onClick={handleRemoveSpacesClick}>Remove Extra Spaces</button>
-                <button className="btn btn-secondary mx-2" onClick={handleCopyClick}>Copy Text</button>
-                <button className="btn btn-danger mx-2" onClick={handleClearClick}>Clear Text</button>
+                <button className={`btn btn${props.mode.backgroundColor === 'light' ? '' : '-outline'}-primary mx-2`} onClick={handleUppercaseClick}>Convert to Uppercase</button>
+                <button className={`btn btn${props.mode.backgroundColor === 'light' ? '' : '-outline'}-success mx-2`} onClick={handleLowercaseClick}>Convert to Lowercase</button>
+                <button className={`btn btn${props.mode.backgroundColor === 'light' ? '' : '-outline'}-dark mx-2`} onClick={handleRemoveSpacesClick}>Remove Extra Spaces</button>
+                <button className={`btn btn${props.mode.backgroundColor === 'light' ? '' : '-outline'}-secondary mx-2`} onClick={handleCopyClick}>Copy Text</button>
+                <button className={`btn btn${props.mode.backgroundColor === 'light' ? '' : '-outline'}-danger mx-2`} onClick={handleClearClick}>Clear Text</button>
             </div>
-            <div className="container my-3" style={{ color: props.mode.backgroundColor === 'light' ? 'white' : 'black' }}>
+            <div className="container my-5" style={{ color: props.mode.backgroundColor === 'light' ? 'white' : 'black' }}>
                 <h2>Your Text Summary</h2>
                 <p>{textarea.length} characters and {textarea.split(" ").length} words</p>
                 <p>{0.008 * textarea.split(" ").length} minutes to read</p>
-                <h3>Preview</h3>
+                <h2>Preview</h2>
                 <p>{textarea}</p>
             </div>
         </>
